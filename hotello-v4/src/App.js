c516dbd2a1d89401cafc66c839ae1932a419e6f4
Mikaela Hedblom <Mikaelahedblom@hotmail.com>
2022-09-25 16:39:31 +0200

    Footern ändring + global färger css

--- hotello-v4/src/App.js
+++ hotello-v4/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Homepage from './pages/Homepage_page';
-
+import Footer from "./comp/Comp_Footer";
 function App() {
   return (
     <div className="App">
     
-       
-
+      
         <Homepage></Homepage>
-
-
+        <Footer></Footer>
 
     </div>
+
+    
   );
 }
 
 export default App;
+
+
