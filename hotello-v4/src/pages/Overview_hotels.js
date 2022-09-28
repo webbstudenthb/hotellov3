9ff1dc506168c92b1f18aa90a5a25356b9978c6c
Joanna Pernu <joanna.pernu@gmail.com>
2022-09-28 13:44:38 +0200

    ändring

--- hotello-v4/src/pages/Overview_hotels.js
+++ hotello-v4/src/pages/Overview_hotels.js
@@ -2,12 +2,15 @@
 
 import Footer from "../comp/Comp_Footer";
 import Header from "../comp/Comp_Header";
+import Hero from "../comp/Comp_Hero";
 
 const Overview = () => {
   return (
     <>
       <Header />
+      <Hero />
       <h3>Overview</h3>
+      
 
       <Footer />
     </>
