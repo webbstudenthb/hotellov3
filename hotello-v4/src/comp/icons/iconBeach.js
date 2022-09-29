bb7f09c3969a2451964a3f65256c1f50f0eb4368
Tim Hallberg <tim.hallberg@outlook.com>
2022-09-29 18:06:47 +0200

    hotelListCards klar samt skapat ikoner. Även CSS för ikoner

---
+++ hotello-v4/src/comp/icons/iconBeach.js
@@ -0,0 +1,17 @@
+import "../../styles/icons.css";
+
+function IconBeach() {
+  return (
+    <div className="iconBoxInline">
+      <div className="iconBox">
+        <img
+          style={{ width: "18px", height: "18px", color: "white" }}
+          src="https://cdn.glitch.global/b8bfc950-5347-4077-bd94-1e2db9ffd2ee/umbrella-beach-solid%20(1).svg?v=1664457891842"
+          alt="Ikon med ett parasoll"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default IconBeach;
---
+++ hotello-v4/src/comp/icons/iconPoolbar.js
@@ -0,0 +1,17 @@
+import "../../styles/icons.css";
+
+function IconPoolbar() {
+  return (
+    <div className="iconBoxInline">
+      <div className="iconBox">
+        <img
+          style={{ width: "18px", height: "18px", color: "white" }}
+          src="https://cdn.glitch.global/b8bfc950-5347-4077-bd94-1e2db9ffd2ee/martini-glass-solid.svg?v=1664459652454"
+          alt="Ikon med ett parasoll"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default IconPoolbar;
